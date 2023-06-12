fc802784800c91ab47b372d470a552fe8a640f01
Syed Muhammad Zain Sarfraz <zainsarfraz82@gmail.com>
2023-06-13 03:50:00 +0500

    Beaches updated with Api work

--- beaches.js
+++ beaches.js
@@ -5,14 +5,16 @@
             let searchField = document.getElementById("searchbar")
             let btn = document.getElementById("btn")
             let rows = document.getElementById("feild")
+            let body = document.getElementById("body1")
 
             function display(result) {
                 rows.innerHTML = ""
+                body.innerHTML = ""
                 result.forEach(value => {
                     rows.innerHTML += `
-                    <div class="col-lg-3 col-md-4 col-sm-6" style="margin-top: 200px !important;">
-                    <div class="card" style="overflow: scroll; box-sizing: border-box; height: 100%;">
+                    <div class="col-lg-3 col-md-4 col-sm-6">
                     <h1 class="card-title" style="text-transform: capitalize;font-family: playfair display;font-style: italic;margin-bottom: 20px !important;">${value.coast}</h1>
+                    <div class="card" style="overflow: scroll; box-sizing: border-box; height: 100%;">
                         <img src="${value.images}" class="card-img-top" alt="..." style="height: 200px;">
                         <div class="card-body">
                           <h5 class="card-title" style="font-family: playfair display;">${value.title}</h5>
